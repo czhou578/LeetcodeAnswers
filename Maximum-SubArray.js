e8ce7f7051f0fe27d2350bee1bdf55ca30e5900e
czhou578 <colizu2020@gmail.com>
2021-10-18 14:25:39 -0700

    finished max subarray with new alg

--- Maximum-SubArray.js
+++ Maximum-SubArray.js
@@ -1,15 +1,20 @@
 var maxSubArray = function(nums) {
-  var sum = 0;
 
-  if (nums.length == 1) {
-    return sum += nums[0]
-  }
-
+  let currentStreak = nums[0]
+  let globalStreak = nums[0];
   
+  for (let i = 1; i < nums.length; i++) {
 
+    currentStreak = Math.max(nums[i], currentStreak + nums[i])
+    
+    if (currentStreak > globalStreak) {
+      globalStreak = currentStreak;
+    }
+  }
+  return globalStreak;
 
+}
 
-};
 
 let test = [-2,1,-3,4,-1,2,1,-5,4];
 
