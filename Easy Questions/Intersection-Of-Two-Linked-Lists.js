8f4c3426b3cfaf68345d587befe3f13e183a8c32
czhou578 <colizu2020@gmail.com>
2021-10-19 16:02:18 -0700

    done

--- Easy Questions/Intersection-Of-Two-Linked-Lists.js
+++ Easy Questions/Intersection-Of-Two-Linked-Lists.js
@@ -12,5 +12,28 @@
  * @return {ListNode}
  */
  var getIntersectionNode = function(headA, headB) {
-    
+  if (headA == null || headB == null) return null
+  
+  let firstPointer = headA
+  let secondPointer = headB
+  
+  while (firstPointer !== secondPointer) {
+      firstPointer = firstPointer.next;
+      secondPointer = secondPointer.next;
+      
+      if (firstPointer === secondPointer) {
+          return firstPointer;
+      }
+      
+      if (firstPointer === null) {
+          firstPointer = headB
+      }
+      
+      if (secondPointer === null) {
+          secondPointer = headA
+      }
+      
+  }
+  
+  return firstPointer;
 };
