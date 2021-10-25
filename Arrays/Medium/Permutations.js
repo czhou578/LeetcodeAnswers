c35cee3d411923c9650c0986bc48c9b9d4d3d1bd
czhou578 <colizu2020@gmail.com>
2021-10-24 17:24:02 -0700

    watched video and did permutations

---
+++ Arrays/Medium/Permutations.js
@@ -0,0 +1,16 @@
+/**
+ * @param {number[]} nums
+ * @return {number[][]}
+ */
+ var permute = function(nums, set=[], answers=[]) {
+  if (!nums.length) answers.push([...set])
+
+  for (let i = 0; i < nums.length; i++) {
+    const tempHold = nums.filter((number, index) => index != i)
+    set.push(nums[i])
+    permute(tempHold, set, answers)
+    set.pop();
+  }
+
+  return answers;
+};
