b63b7e35e72daa79cb0d94e35cfef4cea8688c89
czhou578 <colizu2020@gmail.com>
2022-01-02 18:01:36 -0800

    done

---
+++ BackTracking/Letter-Combo-PhoneNum.js
@@ -0,0 +1,38 @@
+/**
+ * @param {string} digits
+ * @return {string[]}
+ */
+ var letterCombinations = function(digits) {
+  let result = []
+  
+  if (digits.length === 0) return result
+  
+  const map = {
+      '2': 'abc',
+      '3': 'def',
+      '4': 'ghi',
+      '5': 'jkl',
+      '6': 'mno',
+      '7': 'pqrs',
+      '8': 'tuv',
+      '9': 'wxyz'
+  }
+  
+  const dfs = (i, digits, slate) => {
+      if (i === digits.length) {
+          result.push(slate.join(''))
+          return result;
+      }
+      
+      let chars = map[digits[i]]
+      
+      for (let char of chars) {
+          slate.push(char)
+          dfs(i + 1, digits, slate)
+          slate.pop()
+      }
+  }
+  
+  dfs(0, digits, [])
+  return result;
+};
