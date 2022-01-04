6d1d58e5af2cb7b73ec1b7f006d1e1ab0a03d107
czhou578 <colizu2020@gmail.com>
2022-01-04 10:47:57 -0800

    finished problem

---
+++ BackTracking/Combination-Sum.js
@@ -0,0 +1,27 @@
+/**
+ * @param {number[]} candidates
+ * @param {number} target
+ * @return {number[][]}
+ */
+ var combinationSum = function(candidates, target) {
+  let results = []
+  candidates.sort((a,b) => a - b)
+
+  const defs = (i, candidates, target, slate) => {
+    if (target < 0) return
+
+    if (target === 0) {
+      results.push(slate.slice())
+      return;
+    }
+
+    for (let j = i; j < candidates.length; j++) {
+      slate.push(candidates[j])
+      defs(j, candidates, target - candidates[j], slate)
+      slate.pop()
+    }
+  }
+
+  defs(0, candidates, target, [])
+  return results
+};
