638ee6777b707f4b1af6288acfe597a572a095d1
czhou578 <colizu2020@gmail.com>
2022-01-13 11:32:35 -0800

    finished problem

---
+++ Dynamic-Programming/Jump-Game.js
@@ -0,0 +1,15 @@
+/**
+ * @param {number[]} nums
+ * @return {boolean}
+ */
+var canJump = function(nums) {
+  let lastIndex = nums.length - 1;
+
+  for (let i = nums.length - 1; i >= 0; i--) {
+    if (i + nums[i] >= lastIndex) {
+      lastIndex = i
+    }
+  }
+
+  return lastIndex === 0
+};
