cd1e792d53ef7c55ef49899eb98ec0e26884a6f6
czhou578 <colizu2020@gmail.com>
2022-01-17 10:59:28 -0800

    finished problem

---
+++ Dynamic-Programming/Best-Time-To-Buy-And-Sell-Stock.js
@@ -0,0 +1,18 @@
+/**
+ * @param {number[]} prices
+ * @return {number}
+ */
+var maxProfit = function(prices) {
+  let maxProfit = 0;
+  let current_Profit = prices[0]
+  
+  for (let i = 1; i < prices.length; i++) {
+    if (current_Profit < prices[i]) {
+      maxProfit = Math.max(maxProfit, prices[i] - current_Profit)
+    } else {
+      current_Profit = prices[i]
+    }
+  }
+
+  return maxProfit
+};
