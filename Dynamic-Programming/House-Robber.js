ca3dd4c232794b2559535643abc9256ec7685e65
czhou578 <colizu2020@gmail.com>
2022-01-18 21:37:20 -0800

    finished problem

---
+++ Dynamic-Programming/House-Robber.js
@@ -0,0 +1,23 @@
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var rob = function(nums) {
+  if (nums.length == 0 || nums == null) {
+    return 0
+  }  
+
+  if (nums.length == 1) {
+    return nums[0]
+  }
+
+  let result = []
+  result[0] = nums[0]
+  result[1] = Math.max(nums[0], nums[1])
+
+  for (let i = 2; i < nums.length; i++) {
+    result[i] = Math.max(result[i - 1], result[i - 2] + nums[i])
+  }
+
+  return result[result.length - 1]
+};
