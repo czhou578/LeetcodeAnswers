858090547eeaf9835e292f7df7828559ecce9f82
czhou578 <colizu2020@gmail.com>
2022-02-02 16:48:19 -0800

    finished problem

---
+++ Least-Common-Ancestor.js
@@ -0,0 +1,42 @@
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val) {
+ *     this.val = val;
+ *     this.left = this.right = null;
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @param {TreeNode} p
+ * @param {TreeNode} q
+ * @return {TreeNode}
+ */
+var lowestCommonAncestor = function(root, p, q) {
+  // if (root == null) return null
+  let result = null
+  
+  const dfs = (node) => {
+    if (node === null) return false
+
+    // dfs(node.left)
+    // dfs(node.right)
+
+    // if (node.val === q.val || node.val === p.val) {
+    //   return true;
+    // }
+
+    let left = dfs(node.left)
+    let right = dfs(node.right)
+
+    let current = node === p || node === q;
+
+    if (left + right + current >= 2) { //true is 1, false is 0, 
+      result = node
+    }
+
+    return left || right || current
+  }
+
+  dfs(root)
+  return result
+};
