73112ab65692d58c992858b0797369bea3e4311c
czhou578 <colizu2020@gmail.com>
2022-02-04 18:20:55 -0800

    finished problem

---
+++ Trees/Path-Sum-2.js
@@ -0,0 +1,45 @@
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @param {number} targetSum
+ * @return {number[][]}
+ */
+ var pathSum = function(root, targetSum) {
+  if (!root) return []
+
+  let resultArray = []
+
+  const dfs = (node, sum, slate) => {
+    if (node.left === null && node.right === null) {
+      if (sum === node.val) {
+        slate.push(node.val)
+        resultArray.push(slate.slice())
+        slate.pop()
+        // return
+      }
+    }
+
+    // if (node.val !== )
+    if (node.left) {
+      slate.push(node.val)
+      dfs(node.left, sum - node.val, slate)
+      slate.pop()
+    }
+
+    if (node.right) {
+      slate.push(node.val)
+      dfs(node.right, sum - node.val, slate)
+      slate.pop()
+    }
+  }
+
+  dfs(root, targetSum, [])
+  return resultArray
+};
