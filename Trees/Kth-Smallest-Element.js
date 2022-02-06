5f55d50a47a8ae8c9e38c963d6e1d8d6829f61ff
czhou578 <colizu2020@gmail.com>
2022-02-05 17:26:34 -0800

    finished problem

---
+++ Trees/Kth-Smallest-Element.js
@@ -0,0 +1,76 @@
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @param {number} k
+ * @return {number}
+ */
+ var kthSmallest = function(root, k) {
+
+  // let array = [] //push into this array
+  // let counter = 0
+  
+  // const dfs = (node) => {
+    
+  //   if (node.left) {
+  //     array.push(node.left.val)
+  //     dfs(node.left)
+      
+  //   } else {
+  //     array.push(null)
+  //     return
+      
+  //   }
+
+  //   array.push(node.val)
+
+  //   if (node.right) {
+  //     array.push(node.right.val)
+  //     dfs(node.right)
+
+  //   } else {
+  //     array.push(null)
+  //     return
+  //   }
+  // }
+
+  // dfs(root)
+
+  // for (let number of array) {
+  //   if (counter === k) return number
+  //   if (number === null) continue
+  //   counter++
+  // }
+
+  let array = []
+
+  const dfs = (node, array) => {
+    if (!node) {
+      return 
+    }
+
+    dfs(node.left, array) //dfs with left node, and the !node will check to see if it was null
+    array.push(node.val)
+    dfs(node.right, array)
+  }
+
+  dfs(root, array)
+
+  return array[k - 1] //no need to traverse
+};
+
+/**
+ * Similarities: checking null at beginning, push into array, inorder traversal, return from edge case, no adding null
+ * 
+ * Differences: passing array, no need to traverse array, just use index. don't push 
+ * 
+ * Biggest mistake: inorder traversal is wrong. Supposed to go to the bottom of the tree and push, not at the beginning.
+ * 
+ * O(N) time and space complexity
+ */
