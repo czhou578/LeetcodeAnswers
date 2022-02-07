bdd3b2fec2b691709ab1d319ebab7f676ad92e1f
czhou578 <colizu2020@gmail.com>
2022-02-06 17:53:42 -0800

    finished problem

--- Trees/Subtree-of-another-tree.js
+++ Trees/Subtree-of-another-tree.js
@@ -12,5 +12,43 @@
  * @return {boolean}
  */
  var isSubtree = function(root, subRoot) {
-    
+
+  if (!root) {
+    return false
+  } else if (isFound(root, subRoot)) {
+    return true
+  } else {
+    return isSubtree(root.left, subRoot) || isSubtree(root.right, subRoot)
+  }
+
+  // let treeExists = false;
+  
+  // const dfs = (node, node2) => {
+  //   if (node === null || node2 === false) return
+  //   if (node === null && node2 === false) return
+  //   if (node.val === node2.val) {
+  //     treeExists = true
+  //   } else {
+  //     treeExists = false
+  //   }
+
+  //   dfs(node.left, node2.left)
+  //   dfs(node.right, node2.right)
+
+  // }
+
+  // dfs(root, subRoot)
+
+  // return treeExists
+
 };
+
+function isFound(node, node2) {
+  if (node === null || node2 === null) {
+    return 
+  } else if (node.val === node2.val) {
+    return isFound(node.left, node2.left) && isFound(node.right, node2.right)
+  } else {
+    return false
+  }
+}
