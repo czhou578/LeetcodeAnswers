4dc6d4ceca9af0917478501d86685283889950ca
czhou578 <colizu2020@gmail.com>
2022-02-07 18:05:36 -0800

    created new folder

--- Trees DFS/Path-Sum-3.js
+++ Trees DFS/Path-Sum-3.js
@@ -39,3 +39,9 @@ function findSum(node, sum) {
   findSum(node.right, sum - node.val)
 
 }
+
+/**
+ * search and for every new node where traversal starts, traverse that subtree and try to find a path
+ * If the node is null, then there is no need to traverse its left and right subtrees because they don't exist
+ * 
+ */
