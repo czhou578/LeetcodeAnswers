e78f1abad0be58160cae9de8bad2ffcac2198be2
czhou578 <colizu2020@gmail.com>
2022-02-07 18:45:25 -0800

    finished problem

--- Tree BFS/Level-Order-Traversal.js
+++ Tree BFS/Level-Order-Traversal.js
@@ -11,5 +11,29 @@
  * @return {number[][]}
  */
 var levelOrder = function(root) {
-    
+  if (root === null) return []
+
+  let queue = []
+  let result = []
+
+  queue.push(root)
+
+  while (queue.length > 0) {
+    let level = []
+    let length = queue.length //queue.length in the for loop will not update after each iteration, so has to be used as a variable
+
+    for (let i = 0; i < length; i++) {
+      let node = queue.shift() //push the first element into the level array
+      level.push(node.val)
+      if (node.left) queue.push(node.left)
+      if (node.right) queue.push(node.right)
+    }
+
+    result.push(level)
+  }
+
+  return result
+
+
 };
+
