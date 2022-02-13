c03f2ca3457a731313f213c5a98e32645817d5de
czhou578 <colizu2020@gmail.com>
2022-02-13 13:41:20 -0800

    finished problem

---
+++ Tree BFS/Populating-Next-Right-Pointers-II.js
@@ -0,0 +1,40 @@
+/**
+ * // Definition for a Node.
+ * function Node(val, left, right, next) {
+ *    this.val = val === undefined ? null : val;
+ *    this.left = left === undefined ? null : left;
+ *    this.right = right === undefined ? null : right;
+ *    this.next = next === undefined ? null : next;
+ * };
+ */
+
+/**
+ * @param {Node} root
+ * @return {Node}
+ */
+ var connect = function(root) {
+  if (!root) return null
+
+  let queue = [] 
+  
+  queue.push(root)
+
+  while(queue.length > 0) {
+    let length = queue.length
+
+    for (let i = 0; i < length; i++) {
+      let node = queue.shift()
+      if (i !== length - 1) {
+        node.next = queue[0]
+      }
+
+      if (node.left) queue.push(node.left)
+
+      if (node.right) queue.push(node.right)
+    }
+
+  }
+
+  return root
+    
+};
