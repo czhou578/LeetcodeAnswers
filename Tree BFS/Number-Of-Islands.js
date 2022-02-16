6280193d93a40f56cc507988cc79c35659752c97
czhou578 <colizu2020@gmail.com>
2022-02-15 22:12:50 -0800

    finished problem

---
+++ Tree BFS/Number-Of-Islands.js
@@ -0,0 +1,62 @@
+/**
+ * @param {character[][]} grid
+ * @return {number}
+ */
+var numIslands = function(grid) {
+  //edge case is if    
+  const tempGrid = grid.map(row => row.map(column => false))
+  let islandCount = 0
+  
+  for (let i = 0; i < tempGrid.length; i++) {
+    for (let j = 0; j < tempGrid[i].length; j++) {
+      if (dfs(i, j, grid, tempGrid)) islandCount++
+    }
+    
+  }
+
+  return islandCount
+};
+
+const dfs = (i, j, grid, tempGrid) => {
+  const stack = [[i, j]]
+  let islandSize = 0;
+
+  while (stack.length > 0) {
+    let current = stack.pop()
+    let [i, j] = current //destructre i and j
+
+    if (tempGrid[i][j]) continue
+    tempGrid[i][j] = true
+
+    if (grid[i][j] === '0') continue
+    islandSize++;
+
+    let array = isAdjacent(i, j, grid, tempGrid)  
+    stack.push(...array)
+  }
+
+  return islandSize > 0 ? true : false
+}
+
+const isAdjacent = (i, j, grid, tempGrid) => {
+  const adjNeighbors = []
+
+  if (i > 0 && !tempGrid[i - 1][j]) { //is a cell above
+    adjNeighbors.push([i - 1, j])
+  }
+
+  if (i < grid.length - 1 && !tempGrid[i + 1][j]) {
+    adjNeighbors.push([i + 1, j])
+  }
+
+  if (j > 0 && !tempGrid[i][j - 1]) { //is a cell above
+    adjNeighbors.push([i, j - 1])
+  }
+
+  if (j < grid[0].length - 1 && !tempGrid[i][j + 1]) {
+    adjNeighbors.push([i, j + 1])
+  }
+
+
+  return adjNeighbors
+}
