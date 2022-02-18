e580d2d1e659d49d46c4146b578139281bdeea6c
czhou578 <colizu2020@gmail.com>
2022-02-17 21:39:38 -0800

    finished problem

---
+++ Two Pointers/3SumClosest.js
@@ -0,0 +1,31 @@
+/**
+ * @param {number[]} nums
+ * @param {number} target
+ * @return {number}
+ */
+ var threeSumClosest = function(nums, target) {
+  let sorted = nums.sort((a, b) => a - b)
+
+  let closest = nums[0] + nums[1] + nums[2]
+
+  for (let i = 0; i < sorted.length - 2; i++) {
+    let start = i + 1
+    let end = sorted.length - 1
+
+    while(start < end) {
+      let sum = sorted[i] + sorted[start] + sorted[end]
+
+      if (sum > target) {
+        end--;
+      } else {
+        start++
+      }
+
+      if (Math.abs(target - sum) < Math.abs(target - closest)) {
+        closest = sum
+      }
+    }
+  }
+
+  return closest
+};
