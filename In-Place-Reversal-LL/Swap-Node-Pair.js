b6af1450c626da447cbb4f1234460ac0d34081d0
czhou578 <colizu2020@gmail.com>
2022-03-01 20:01:41 -0800

    comment

--- In-Place-Reversal-LL/Swap-Node-Pair.js
+++ In-Place-Reversal-LL/Swap-Node-Pair.js
@@ -21,5 +21,13 @@
 
   return head
 
-
 };
+
+/**
+ * Use recursion to solve swapPairs
+ * What I didn't get: couldn't picture the swapping of two nodes. Think of 3 people in line, and the person at the back reaches out to grab the
+ * hand of the first in line. The person in the middle reaches back to grab the back person's free hand and lets go of the hand of the person at the front.
+ * 
+ * 
+ * Takdaways: Recursion is always an option! 
+ */
