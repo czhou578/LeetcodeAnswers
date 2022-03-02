49d30468245d6b942bfabb8fbdc6bb39f9517793
czhou578 <colizu2020@gmail.com>
2022-03-02 10:15:31 -0800

    finished problem

--- Dynamic-Programming/Maximum-Product-Subarray.js
+++ Dynamic-Programming/Maximum-Product-Subarray.js
@@ -5,24 +5,45 @@
  var maxProduct = function(nums) {
   if (nums.length === 1) return nums[0]
 
-  let array = nums.map((element) => element)
+  // let array = nums.map((element) => element)
 
-  for (let i = 1; i < nums.length; i++) {
-    let temp = nums[i]
-    for (let j = 0; j < i; j++) {
+  // for (let i = 1; i < nums.length; i++) {
+  //   let temp = nums[i]
+  //   for (let j = 0; j < i; j++) {
 
-      if (nums[i] * nums[j] < array[i] && i - j == 1)
+  //     if (nums[i] * nums[j] < array[i] && i - j == 1)
       
-      if (nums[i] * nums[j] < array[i] && i - j == 1) {
-        array[i] = temp
-        continue
-      } else if (nums[i] * nums[j] >= nums[i]) {
-        array[i] = Math.max(array[i], nums[i] * nums[j])
-      }
+  //     if (nums[i] * nums[j] < array[i] && i - j == 1) {
+  //       array[i] = temp
+  //       continue
+  //     } else if (nums[i] * nums[j] >= nums[i]) {
+  //       array[i] = Math.max(array[i], nums[i] * nums[j])
+  //     }
+  //   }
+  // }
+
+  // return Math.max(...array)
+
+  let result = Math.max(...nums)
+  let minProduct = 1
+  let maxProduct = 1
+
+  for (const number of nums) {
+    if (number == 0) {
+      minProduct = 1
+      maxProduct = 1
+      continue
     }
+
+    let temp = maxProduct * number
+
+    maxProduct = Math.max(number * minProduct, number * maxProduct, number)
+    minProduct = Math.min(temp, number * minProduct, number)
+    result = Math.max(result, maxProduct)
   }
 
-  return Math.max(...array)
+  return result
+
 };
 
 let test = [-2, 3, -1]
