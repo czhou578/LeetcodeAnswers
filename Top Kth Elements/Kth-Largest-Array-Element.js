74ba9613221a63e86279632fac1dbf993826fef3
czhou578 <colizu2020@gmail.com>
2022-03-06 19:21:23 -0800

    changed code

--- Top Kth Elements/Kth-Largest-Array-Element.js
+++ Top Kth Elements/Kth-Largest-Array-Element.js
@@ -6,25 +6,25 @@
  var findKthLargest = function(nums, k) {
   if (nums.length == 1) return nums[0]
 
-  let tempK = k - 1
+  /**
+   * Variable temp starts at 1 and represents the kth largest element currently, which will
+   * be incremented every time we loop.
+   * 
+   * If the temp is less then k, then we increment variable temp.
+   */
+
   let temp = 1;
 
   nums.sort((a, b) => b - a) //sort descending
 
   if (k == 1) return nums[0]
 
-  for (let i = 0; i < nums.length - 1; i++) {
-    let firstPointer = nums[i]
-    let secondPointer = nums[i + 1]
+  for (let i = 0; i < nums.length; i++) {
 
-    if (temp == tempK) return secondPointer
-    
-    if (firstPointer !== secondPointer && temp < tempK || firstPointer === secondPointer) {
-      temp++
+    if (temp == k) {
+      return nums[i]
     }
-
+    
+    temp++
   }
 };
-
-let test = [-1,2,0]
-console.log(findKthLargest(test, 1));
