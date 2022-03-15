eedc7c87af8c56f16e0b17b3012fcc6aefa605da
czhou578 <colizu2020@gmail.com>
2022-03-14 21:53:51 -0700

    commit

---
+++ Arrays/Medium/Duplicates-In-Array.js
@@ -0,0 +1,30 @@
+/**
+ * @param {number[]} nums
+ * @return {number[]}
+ */
+ var findDuplicates = function(nums) {
+  // if (nums.length == 1) return []
+
+  // let results = []
+  // let temp = nums[nums.length - 1]
+
+  // for (let i = nums.length - 2; i >= 0; i++) {
+  //   if (nums[i])
+  // }
+  let map = new Map()
+
+  let results = []
+
+  nums.forEach((element) => {
+    if (!map.has(element)) {
+      map.set(element, 1)
+    } else {
+      results.push(element)
+    }
+  })
+
+  return results
+
+};
+
+//used map to solve this
