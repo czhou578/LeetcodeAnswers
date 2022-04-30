5570d9c86cb1817060a51a2f8e38318a239be9f9
czhou578 <colizu2020@gmail.com>
2022-04-30 12:14:17 -0700

    finished problem

---
+++ Bits/Sum-Of-Two-Integers.js
@@ -0,0 +1,17 @@
+/**
+ * @param {number} a
+ * @param {number} b
+ * @return {number}
+ */
+ var getSum = function(a, b) {
+    if (b == 0) {
+      return a
+    } else {
+      return getSum(a^b, (a&&b)<<1)
+    }
+};
+
+/**
+ * time complexity: O(n)
+ * space complexity: O(1)
+ */
