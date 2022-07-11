bb14c68f7003243d3063876bb1f848d56e87689b
czhou578 <colizu2020@gmail.com>
2022-07-10 18:42:51 -0700

    commit

--- Dynamic-Programming/Longest-common-Subsequence.js
+++ Dynamic-Programming/Longest-common-Subsequence.js
@@ -3,22 +3,24 @@
  * @param {string} text2
  * @return {number}
  */
- var longestCommonSubsequence = function(text1, text2) {
+var longestCommonSubsequence = function (text1, text2) {
   if (text1 == "" || text2 == "") return 0;
   // The first row and the first col is an empty string (base case).
   // dp[row][col] represents the length of the longest possible subsequence
   // using substrings text1.substring(0,row) and text2.substring(0,col).
-  let dp = new Array(text2.length+1).fill(0).map(() => new Array(text1.length+1));
+  let dp = new Array(text2.length + 1)
+    .fill(0)
+    .map(() => new Array(text1.length + 1));
   for (let row = 0; row < dp.length; row++) {
-      for (let col = 0; col < dp[0].length; col++) {
-          if (row == 0 || col == 0) {
-              dp[row][col] = 0; // can't have a common subsequence with ""
-          } else if (text1[col-1] === text2[row-1]) {
-              dp[row][col] = dp[row-1][col-1] + 1;
-          } else {
-              dp[row][col] = Math.max(dp[row][col-1], dp[row-1][col]);
-          }
+    for (let col = 0; col < dp[0].length; col++) {
+      if (row == 0 || col == 0) {
+        dp[row][col] = 0; // can't have a common subsequence with ""
+      } else if (text1[col - 1] === text2[row - 1]) {
+        dp[row][col] = dp[row - 1][col - 1] + 1;
+      } else {
+        dp[row][col] = Math.max(dp[row][col - 1], dp[row - 1][col]);
       }
+    }
   }
   return dp[text2.length][text1.length];
 };
--- Sliding-Window/Longest-Repeating-Char-Replacement.js
+++ Sliding-Window/Longest-Repeating-Char-Replacement.js
@@ -3,75 +3,27 @@
  * @param {number} k
  * @return {number}
  */
- var characterReplacement = function(s, k) {
-  // let replaceCount = k
-  // let longestSubstringLength = -Infinity;
-  
-  // for (let i = 0; i < s.length - k; i++) {
-  //   let firstPointer = i
-  //   let firstPointerValue = s[i]
-  //   let iteratorLength = 1; //length of longest substring in current iteration
-    
-  //   for (let j = firstPointer + 1; j < s.length; j++) {
-  //     if (replaceCount < 0 && s[j] != firstPointerValue) break
-
-  //     if (s[j] !== firstPointerValue) {
-  //       iteratorLength++
-  //       replaceCount--
-  //     } else {
-  //       iteratorLength++
-  //     }
-  //   }
-
-  //   longestSubstringLength = Math.max(longestSubstringLength, iteratorLength)
-  // }
-
-  // return longestSubstringLength
-
-  // let mostFrequent = -Infinity
-  // let maximum = -Infinity
-  // let charArray = new Array(26).fill(0)
-  // let start = 0;
-
-  // for (let i = 0; i < s.length; i++) { //i will act as the furthest pointer ahead
-  //   let index = s.charCodeAt(i) - 65
-  //   charArray[index] += 1
-  //   mostFrequent = Math.max(mostFrequent, charArray[index])
-  //   let charsToModify = (i - start + 1) - mostFrequent
-
-  //   if (charsToModify > k) {
-  //     let index2 = s.charCodeAt(start) - 65
-  //     charArray[index2] -= 1
-  //     start++
-  //   }
-
-  //   maximum = Math.max(maximum, i - start + 1)
-
-  // }
-
-  // return maximum
-
-  let count = {}
-  let result = 0
-  let left = 0
+var characterReplacement = function (s, k) {
+  let count = {};
+  let result = 0;
+  let left = 0;
 
   for (let right = 0; right < s.length; right++) {
     if (count[s[right]]) {
-      count[s[right]] = 1 + count[s[right]]
+      count[s[right]] = 1 + count[s[right]];
     } else {
-      count[s[right]] = 0
+      count[s[right]] = 0;
     }
 
-    while ((right - left + 1) - Math.max(Object.values(count)) > k) {
-      count[s[left]] -= 1
-      left += 1
+    while (right - left + 1 - Math.max(Object.values(count)) > k) {
+      count[s[left]] -= 1;
+      left += 1;
     }
 
-    result = Math.max(result, right - left + 1)
+    result = Math.max(result, right - left + 1);
   }
 
-  return result
-
+  return result;
 };
 
 /**
@@ -79,7 +31,7 @@
  * O(1) space complexity
  */
 
-let string1 = "ABBB"
-let num = 3
+let string1 = "ABBB";
+let num = 3;
 
 console.log(characterReplacement(string1, num));
