53fcbdba39e572ff5c72b3e0c3c80a9c5308c926
czhou578 <colizu2020@gmail.com>
2022-10-11 10:04:59 -0500

    commit

--- Cracking-Coding-Interview.js
+++
@@ -1,5 +0,0 @@
-//Dynamic programming
-
-//recursive, iterative and tradeoffs.
-
-//O(2^n)
--- Hash Table/Easy/Longest-Palindrome.js
+++ Hash Table/Easy/Longest-Palindrome.js
@@ -2,8 +2,17 @@
  * @param {string} s
  * @return {number}
  */
-var longestPalindrome = function(s) {
-  if (s.length === 1) return true
-  
-  
+var longestPalindrome = function (s) {
+  if (s.length === 1) return true;
+
+  let map = new Map();
+
+  for (let x of s) {
+    if (!map.has(x)) map.set(x, 1);
+    else map.delete(x);
+  }
+
+  let array = Array.from(map.values());
+  if (array.length <= 1) return s.length;
+  else return s.length - array.length + 1;
 };
